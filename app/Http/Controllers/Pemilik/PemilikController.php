3fdb41e78fcf32b09a61b0708f031e83b6ca989c
mulyadirizki <mulyadirizkiputra10@gmail.com>
2024-07-24 09:29:58 +0700

    update

--- app/Http/Controllers/Pemilik/PemilikController.php
+++ app/Http/Controllers/Pemilik/PemilikController.php
@@ -26,33 +26,52 @@ class PemilikController extends Controller
         return view('pemilik.home', compact('user', 'booking', 'transaksi'));
     }
 
-    public function pemilikPemainPage()
+    public function pemilikPemainPage(Request $request)
     {
         $data = T_user::join('users', 't_user.id_tuser', '=', 'users.id_tuser')
             ->select('t_user.*', 'users.roles',
                 (DB::raw('(CASE WHEN t_user.j_kel = 1 THEN "Laki - Laki" WHEN t_user.j_kel = 2 THEN "Perempuan" ELSE "-" END) as jkel')))
             ->where('users.roles', 3)
             ->get();
-        return view('pemilik.data.p_pemain', compact('data'));
+
+        $userName = auth()->user()->username;
+
+        if ($request->ajax()) {
+            return response()->json($data);
+        }
+
+        return view('pemilik.data.p_pemain', compact('data', 'userName'));
     }
 
-    public function pemilikBookingPage()
+    public function pemilikBookingPage(Request $request)
     {
         $data = Booking::join('t_user', 'm_booking.id_tuser', '=', 't_user.id_tuser')
                     ->join('m_lapangan', 'm_booking.id_lapangan', '=', 'm_lapangan.id_lapangan')
-                    ->select('m_booking.*', 't_user.nama', 'm_lapangan.dsc_lapangan', 'm_lapangan.harga_lapangan',
+                    ->leftJoin('t_transaksi', 't_transaksi.id_booking', '=', 'm_booking.id_booking')
+                    ->select('m_booking.*', 't_user.nama', 'm_lapangan.dsc_lapangan', 'm_lapangan.harga_lapangan', 't_transaksi.id_transaksi',
                         (DB::raw('(CASE WHEN m_booking.status = 1 THEN "Segera Main" WHEN m_booking.status = 2 THEN "Sedang Main" WHEN m_booking.status = 3 THEN "Selesai" ELSE "-" END) as status_booking')),
                         (DB::raw('TIMEDIFF(m_booking.jam_berakhir, m_booking.jam_mulai) as diff')))
                     ->get();
-        return view('pemilik.data.p_booking', compact('data'));
+        $userName = auth()->user()->username;
+
+        if ($request->ajax()) {
+            return response()->json($data);
+        }
+        return view('pemilik.data.p_booking', compact('data', 'userName'));
     }
 
-    public function pemilikTransaksiPage()
+    public function pemilikTransaksiPage(Request $request)
     {
         $data = Pembayaran::join('t_user', 't_transaksi.id_tuser', '=', 't_user.id_tuser')
                     ->join('m_transaksi', 't_transaksi.id_mtransaksi', '=', 'm_transaksi.id_mtransaksi')
                     ->select('t_transaksi.*', 't_user.nama', 'm_transaksi.jenis_bank', 't_user.nama', 'm_transaksi.no_rek')
                     ->get();
-        return view('pemilik.data.p_transaksi', compact('data'));
+
+        $userName = auth()->user()->username;
+
+        if ($request->ajax()) {
+            return response()->json($data);
+        }
+        return view('pemilik.data.p_transaksi', compact('data', 'userName'));
     }
 }
--- resources/views/pemilik/data/p_booking.blade.php
+++ resources/views/pemilik/data/p_booking.blade.php
@@ -22,7 +22,10 @@
                         <div class="card shadow">
                             <div class="card-body">
                                 <!-- table -->
-                                <table class="table table-striped table-bordered nowrap" cellspacing="0" width="100%" id="dataTable-1">
+                                <div>
+                                    <button class="btn btn-info" id="generate-pdf">PDF</button>
+                                </div><br>
+                                <table class="table table-striped table-responsive table-bordered nowrap" cellspacing="0" width="100%" id="dataTable-1">
                                     <thead class="thead-dark">
                                         <tr>
                                             <th>No</th>
@@ -35,31 +38,9 @@
                                             <th>Lama Main</th>
                                             <th>Total Biaya</th>
                                             <th>Status</th>
-                                            <th>Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                      @foreach($data as $index => $item)
-                                            <tr>
-                                                <td>{{ $index + 1}}</td>
-                                                <td>{{ $item->nama }}</td>
-                                                <td>{{ $item->dsc_lapangan }}</td>
-                                                <td>Rp. {{ number_format($item->harga_lapangan) }} /Jam</td>
-                                                <td>{{ $item->tgl_booking }}</td>
-                                                <td>{{ substr($item->jam_mulai, 0, 5) }} WIB</td>
-                                                <td>{{ substr($item->jam_berakhir, 0, 5) }} WIB</td>
-                                                <td>{{ substr($item->diff, 0, 2) }} Jam</td>
-                                                <td>Rp. {{ number_format($item->total_biaya) }}</td>
-                                                <td><span class="badge badge-primary">{{ $item->status_booking }}</span></td>
-                                                <td><button class="btn btn-sm dropdown-toggle more-horizontal" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    <span class="text-muted sr-only">Action</span>
-                                                    </button>
-                                                    <div class="dropdown-menu dropdown-menu-right">
-                                                      <a class="dropdown-item" href="#">Remove</a>
-                                                    </div>
-                                                </td>
-                                            </tr>
-                                        @endforeach
                                     </tbody>
                                 </table>
                             </div>
@@ -71,26 +52,125 @@
     </div> <!-- .container-fluid -->
 @endsection
 @push('script')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.13/jspdf.plugin.autotable.min.js"></script>
     <script>
-      $(document).ready(function() {
-          $('#dataTable-1').DataTable( {
-              dom: 'Bfrtip',
-              buttons: [
-                  'copy', 'csv', 'excel', 'pdf', 'print'
-              ],
-              "lengthMenu": [
-                [16, 32, 64, -1],
-                [16, 32, 64, "All"]
-              ],
-              scrollX:        true,
-              scrollCollapse: true,
-              autoWidth:         true,
-              paging:         true,
-              columnDefs: [
-                { "width": "200px", "targets": [1] },
-                { "width": "100px", "targets": [2, 3, 4, 5, 8] }
-              ]
-          } );
-      } );
+        var userName = "{{ $userName }}";
+        $(document).ready(function() {
+            $.ajax({
+                url: "{{ route('pemilikBooking') }}",  // URL endpoint dari Laravel
+                method: 'GET',
+                success: function(data) {
+                    var tbody = $('#dataTable-1 tbody');
+                    var no = 1;
+                    data.forEach(function(item, index) {
+                        var statusBayar = item.id_transaksi ? '<span class="badge badge-success">Sudah Bayar</span>' : '<span class="badge badge-warning">Belum Bayar</span>';
+                        var row = `<tr>
+                            <td>${no++}</td>
+                            <td>${item.nama}</td>
+                            <td>${item.dsc_lapangan}</td>
+                            <td>Rp. ${number_format(item.harga_lapangan)} /Jam</td>
+                            <td>${item.tgl_booking}</td>
+                            <td>${substr(item.jam_mulai, 0, 5)} WIB</td>
+                            <td>${substr(item.jam_berakhir, 0, 5)} WIB</td>
+                            <td>${substr(item.diff, 0, 2)} Jam</td>
+                            <td>Rp. ${number_format(item.total_biaya)}</td>
+                            <td>${statusBayar}</td>
+                        </tr>`;
+                        tbody.append(row);
+                    });
+
+                    // Inisialisasi DataTables setelah data dimuat
+                    $('#dataTable-1').DataTable();
+                },
+                error: function(error) {
+                    console.error("Error fetching data", error);
+                }
+            });
+
+            function number_format(number, decimals, dec_point, thousands_sep) {
+                number = (number + '').replace(/[^0-9+\-Ee.]/g, '');
+                var n = !isFinite(+number) ? 0 : +number,
+                    prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+                    sep = (typeof thousands_sep === 'undefined') ? ',' : thousands_sep,
+                    dec = (typeof dec_point === 'undefined') ? '.' : dec_point,
+                    s = '',
+                    toFixedFix = function(n, prec) {
+                        var k = Math.pow(10, prec);
+                        return '' + Math.round(n * k) / k;
+                    };
+                s = (prec ? toFixedFix(n, prec) : '' + Math.round(n)).split('.');
+                if (s[0].length > 3) {
+                    s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+                }
+                if ((s[1] || '').length < prec) {
+                    s[1] = s[1] || '';
+                    s[1] += new Array(prec - s[1].length + 1).join('0');
+                }
+                return s.join(dec);
+            }
+
+            function substr(str, start, length) {
+                return str.substring(start, start + length);
+            }
+
+            $('#generate-pdf').click(function() {
+                const { jsPDF } = window.jspdf;
+                const doc = new jsPDF();
+
+                var img = new Image();
+                img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDIMYVJUja6yxCNZ8yQebiIRf8SQfeqArY2Q&s";  // Ganti dengan path logo Anda
+                img.onload = function() {
+                    // Lebar halaman
+                    var pageWidth = doc.internal.pageSize.getWidth();
+                    var pageHeight = doc.internal.pageSize.getHeight();
+
+                    // Lebar gambar
+                    var imgWidth = 50;
+                    var imgHeight = 30;
+
+                    // Posisi gambar di sebelah kiri
+                    var imgX = 10;
+
+                    // Tambahkan gambar (logo) di sebelah kiri
+                    doc.addImage(img, 'PNG', imgX, 15, imgWidth, imgHeight);
+
+                    // Tambahkan teks di tengah sejajar dengan gambar
+                    doc.setFontSize(15);
+                    doc.text("Graha Futsal", pageWidth / 2, 25, null, null, 'center');
+                    doc.text("Jln. By Pass Taluak Ampek Suku", pageWidth / 2, 32, null, null, 'center');
+                    doc.text("LAPORAN BOOKING", pageWidth / 2, 45, null, null, 'center');
+
+                    // Offset untuk tabel agar tidak bertumpuk dengan teks
+                    var startY = 55;
+
+                    var columns = ["No", "Nama Pemain", "Lapangan", "Harga Lapangan", "Tgl Booking", "Jam Mulai", "Jam Berakhir", "Lama Main", "Total Biaya", "Status"];
+                    var rows = [];
+
+                    $('#dataTable-1 tbody tr').each(function(index, element) {
+                        var rowData = [];
+                        $(element).find('td').each(function(index, td) {
+                            rowData.push($(td).text());
+                        });
+                        rows.push(rowData);
+                    });
+
+                    // Menggunakan jsPDF-AutoTable untuk membuat tabel di PDF
+                    doc.autoTable({
+                        head: [columns],
+                        body: rows,
+                        startY: startY // Posisi mulai tabel
+                    });
+
+                    // Tambahkan tanggal di pojok kanan bawah
+                    var currentDate = new Date();
+                    var dateString = 'Bukittinggi, ' + ' ' + currentDate.toLocaleDateString(); // Format tanggal
+                    doc.setFontSize(10);
+                    doc.text(dateString, pageWidth - 10, pageHeight - 35, null, null, 'right');
+                    doc.text(userName, pageWidth - 22, pageHeight - 10, null, null, 'right');
+                    doc.save("Laporan_Booking.pdf");
+                };
+            });
+        });
     </script>
 @endpush
